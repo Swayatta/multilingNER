01b1545ac85248a5e8010e24a661de0345a49e62
root <swayatta@gmail.com>
2021-09-08 10:47:27 +0000

    added corpus

--- bert-crf4NER/config.py
+++ bert-crf4NER/config.py
@@ -2,14 +2,15 @@ class Config(object):
   apr_dir = '../model/'
   data_dir = '../corpus/'
   model_name = 'model_4.pt'
-  epoch = 5
-  bert_model = 'bert-base-uncased'
+  epoch = 1
+  bert_model = 'allenai/scibert_scivocab_uncased'
   lr = 5e-5
   eps = 1e-8
   batch_size = 10
   mode = 'prediction' # for prediction mode = "prediction"
-  training_data = 'biotags_train.txt'
-  val_data = 'biotags_valid.txt'
+  training_data = 'trainbio.txt'
+  val_data = 'trainbio.txt'
   test_data = 'biotags_valid.txt'
   test_out = 'test_prediction.csv'
   raw_prediction_output = 'raw_prediction.csv'
+  hidden_dropout_prob = 0.5
